304ed9b29edc621b58e27c29039009bc0d00736b
Rinart73 <rinart73@gmail.com>
2019-08-31 02:35:22 +0200

    1.0.5 * Now dipslays alliance resources when you're in alliance ship * Hides extra UI when you're in building mode and strategy mode (0.26+ only)

--- data/scripts/player/client/musiccoordinator.lua
+++ data/scripts/player/client/musiccoordinator.lua
@@ -1,7 +1,14 @@
 -- While my mod has nothing to do with music, this approach allows it to be a 100% clientside mod
+
+local resourceDisplay_initialize -- extended functions
+local resourceDisplay_gameVersion
+
 if onClient() then
 
-local resourceDisplay_initialize = MusicCoordinator.initialize
+
+resourceDisplay_gameVersion = GameVersion()
+
+resourceDisplay_initialize = MusicCoordinator.initialize
 function MusicCoordinator.initialize(...)
     resourceDisplay_initialize(...)
 
@@ -12,23 +19,37 @@ end
 
 function MusicCoordinator.resourceDisplay_onPreRenderHud()
     local player = Player()
-    if player.infiniteResources then return end
+    local faction = player.craft.allianceOwned and Alliance() or player
+    local playerNotFlying = false
+    if resourceDisplay_gameVersion.minor >= 26 then
+        playerNotFlying = player.state ~= PlayerStateType.Fly
+    end
+    if faction.infiniteResources or playerNotFlying then return end
 
-    local resources = {player:getResources()}
+    local resources = {faction:getResources()}
     local y = 10
     local material, rect
     for i = 1, #resources do
         material = Material(i-1)
         y = y + 18
         rect = Rect(5, y, 295, y + 16)
-        drawTextRect(material.name, rect, -1, -1, material.color, 15, 0, 0, 2)
+        if faction.isAlliance then
+            drawTextRect("[A]  /* Alliance resource prefix */"%_t..material.name, rect, -1, -1, material.color, 15, 0, 0, 2)
+        else
+            drawTextRect(material.name, rect, -1, -1, material.color, 15, 0, 0, 2)
+        end
         drawTextRect(createMonetaryString(resources[i]), rect, 1, -1, material.color, 15, 0, 0, 2)
     end
     y = y + 18
     rect = Rect(5, y, 295, y + 16)
     local color = ColorRGB(1, 1, 1)
-    drawTextRect("Credits"%_t, rect, -1, -1, color, 15, 0, 0, 2)
-    drawTextRect(createMonetaryString(player.money), rect, 1, -1, color, 15, 0, 0, 2)
+    if faction.isAlliance then
+        drawTextRect("[A]  /* Alliance resource prefix */"%_t.."Credits"%_t, rect, -1, -1, color, 15, 0, 0, 2)
+    else
+        drawTextRect("Credits"%_t, rect, -1, -1, color, 15, 0, 0, 2)
+    end
+    drawTextRect(createMonetaryString(faction.money), rect, 1, -1, color, 15, 0, 0, 2)
 end
+
 
 end
