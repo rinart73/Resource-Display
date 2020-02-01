bb9de07f941e26375c677a3d50d8db849a2b6cbd
Rinart73 <rinart73@gmail.com>
2020-02-01 11:58:30 +0100

    1.1 * Pushed supported version to 0.30.2 * Now depends on AzimuthLib * Added clientside config file * Removed unnecessary overlaying rendering * Now shows current faction inventory capacity (can be turned off in config)

--- data/scripts/player/client/musiccoordinator.lua
+++ data/scripts/player/client/musiccoordinator.lua
@@ -1,17 +1,33 @@
 -- While my mod has nothing to do with music, this approach allows it to be a 100% clientside mod
 
-local resourceDisplay_initialize -- extended functions
+local Azimuth -- client includes
+local resourceDisplay_initialize -- client, extended functions
+local ResourceDisplayConfig, resourceDisplay_hud -- client
 
 if onClient() then
 
+
+Azimuth = include("azimuthlib-basic")
 
 resourceDisplay_initialize = MusicCoordinator.initialize
 function MusicCoordinator.initialize(...)
     resourceDisplay_initialize(...)
 
-    if not GameSettings().infiniteResources then
-        Player():registerCallback("onPreRenderHud", "resourceDisplay_onPreRenderHud")
+    -- load config
+    local configOptions = {
+      _version = { default = "1.0", comment = "Config version. Don't touch." },
+      ShowCargoCapacity = { default = true, comment = "Show current ship cargo capacity" },
+      ShowInventoryCapacity = { default = true, comment = "Show currently used and total inventory slots" },
+      InventoryCapacityShowBothAlways = { default = false, comment = "Show both player and alliance inventory capacity no matter the ship" }
+    }
+    local isModified
+    ResourceDisplayConfig, isModified = Azimuth.loadConfig("ResourceDisplay", configOptions)
+    if isModified then
+        Azimuth.saveConfig("ResourceDisplay", ResourceDisplayConfig, configOptions)
     end
+
+    resourceDisplay_hud = Hud()
+    Player():registerCallback("onPreRenderHud", "resourceDisplay_onPreRenderHud")
 end
 
 function MusicCoordinator.resourceDisplay_onPreRenderHud()
@@ -23,7 +39,7 @@ function MusicCoordinator.resourceDisplay_onPreRenderHud()
     if player.state ~= PlayerStateType.Fly and player.state ~= PlayerStateType.Interact then return end
 
     local y = 10
-    if not faction.infiniteResources and player.state == PlayerStateType.Fly then
+    if not faction.infiniteResources and player.state == PlayerStateType.Fly and not resourceDisplay_hud.resourcesVisible then
         local resources = {faction:getResources()}
         local material, rect
         for i = 1, #resources do
@@ -47,18 +63,46 @@ function MusicCoordinator.resourceDisplay_onPreRenderHud()
         end
         drawTextRect("¢"..createMonetaryString(faction.money), rect, 1, -1, color, 15, 0, 0, 2)
     else
-        y = y + NumMaterials() * 18
+        if resourceDisplay_hud.resourcesVisible then
+            y = y + NumMaterials() * 18
+        end
+    end
+    -- inventory slots
+    if ResourceDisplayConfig.ShowInventoryCapacity then
+        if ResourceDisplayConfig.InventoryCapacityShowBothAlways then
+            faction = player
+        end
+        local inv = faction:getInventory()
+        y = y + 18
+        rect = Rect(5, y, 295, y + 16)
+        color = ColorRGB(0.8, 0.8, 0.8)
+        if faction.isAlliance then
+            drawTextRect("[A]  /* Alliance resource prefix */"%_t.."Inventory Slots"%_t, rect, -1, -1, color, 15, 0, 0, 2)
+        else
+            drawTextRect("Inventory Slots"%_t, rect, -1, -1, color, 15, 0, 0, 2)
+        end
+        drawTextRect(inv.occupiedSlots.."/"..inv.maxSlots, rect, 1, -1, color, 15, 0, 0, 2)
+        if ResourceDisplayConfig.InventoryCapacityShowBothAlways and player.alliance then
+            inv = player.alliance:getInventory()
+            y = y + 18
+            rect = Rect(5, y, 295, y + 16)
+            color = ColorRGB(0.8, 0.8, 0.8)
+            drawTextRect("[A]  /* Alliance resource prefix */"%_t.."Inventory Slots"%_t, rect, -1, -1, color, 15, 0, 0, 2)
+            drawTextRect(inv.occupiedSlots.."/"..inv.maxSlots, rect, 1, -1, color, 15, 0, 0, 2)
+        end
     end
     -- cargo
-    local ship = getPlayerCraft()
-    y = y + 18
-    rect = Rect(5, y, 295, y + 16)
-    color = ColorRGB(0.8, 0.8, 0.8)
-    drawTextRect("Cargo Hold"%_t, rect, -1, -1, color, 15, 0, 0, 2)
-    if ship and ship.maxCargoSpace then
-        drawTextRect(math.ceil(ship.occupiedCargoSpace).."/"..math.floor(ship.maxCargoSpace), rect, 1, -1, color, 15, 0, 0, 2)
-    else
-        drawTextRect("-", rect, 1, -1, color, 15, 0, 0, 2)
+    if ResourceDisplayConfig.ShowCargoCapacity then
+        local ship = getPlayerCraft()
+        y = y + 18
+        rect = Rect(5, y, 295, y + 16)
+        color = ColorRGB(0.8, 0.8, 0.8)
+        drawTextRect("Cargo Hold"%_t, rect, -1, -1, color, 15, 0, 0, 2)
+        if ship and ship.maxCargoSpace then
+            drawTextRect(math.ceil(ship.occupiedCargoSpace).."/"..math.floor(ship.maxCargoSpace), rect, 1, -1, color, 15, 0, 0, 2)
+        else
+            drawTextRect("-", rect, 1, -1, color, 15, 0, 0, 2)
+        end
     end
 end
 
